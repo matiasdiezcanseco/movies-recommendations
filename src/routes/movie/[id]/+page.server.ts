63c1442c7845ae7aa9ef766f56c664faad33d5a0
Matias <matiasdiezcanseco@gmail.com>
2024-10-25 18:40:16 -0500

    removed prerender due to error

--- src/routes/movie/[id]/+page.server.ts
+++ src/routes/movie/[id]/+page.server.ts
@@ -2,8 +2,6 @@ import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types.js';
 import { getMovieById, getRecommendedMoviesByVector } from '../../../server/movies.js';
 
-export const prerender = true;
-
 export const load: PageServerLoad = async ({ params, depends }) => {
 	const id = params.id;
 	depends(`movie:${id}`);
